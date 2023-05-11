21e4922ae167d6c0795bbba49e89cdea9e66a23c
AymanSamy20 <115373997+AymanSamy20@users.noreply.github.com>
2023-05-12 02:42:52 +0300

    Done

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(MainActivity.this, SlideActivity.class);
+                    startActivity(intent);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
     }
 }
